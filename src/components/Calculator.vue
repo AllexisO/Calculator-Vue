42659478f132befae0c2bcab9268f68abb3dc12e
Alexandru Paniciuc <shuricpan@gmail.com>
2018-09-03 14:45:11 +0300

    Starting project

---
+++ src/components/Calculator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="calculator">
+    <div class="display">00112233</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.calculator{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
